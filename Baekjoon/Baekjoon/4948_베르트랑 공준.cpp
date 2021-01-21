2d4a175497b4bbf28de53789687b1f6be7f81e04
ije000131@gmail.com <ije000131@gmail.com>
2021-01-21 23:24:26 +0900

    algo 수정

--- Baekjoon/Baekjoon/4948_베르트랑 공준.cpp
+++ Baekjoon/Baekjoon/4948_베르트랑 공준.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
-using namespace std;
 
-int main()
+bool list[123456 * 2 + 1];
+int n, n2 = 123456 * 2 + 1;
+
+int main() 
 {
-	bool list[246913] = { 0, };
-	int n, cnt;
+	list[0] = list[1] = 1;
+
+	// list 초기화
+	for (int i = 2; i * i <= n2; i++)
+	{
+		if (!list[i])
+		{
+			// 소수의 배수 제거
+			for (int j = i * i; j <= n2; j += i)
+			{
+				list[j] = 1;
+			}
+		}
+	}
 
 	while (true)
 	{
-		cin >> n;
-		cnt = 0;
-
+		int cnt = 0;
+		std::cin >> n;
+		
+		// 0 입력시 종료
 		if (n == 0)
 			break;
 
+		// n 초과 2n 이하 소수의 개수 출력
 		for (int i = n + 1; i <= 2 * n; i++)
 		{
-			list[i] = true;
+			if (!list[i])
+				cnt++;
 		}
-
-		for (int i = 2; i <= 2 * n; i++)
-		{
-			for (int j = 2 * i; j <= 2 * n; j += i)
-			{
-				list[j] = false;
-			}
-		}
-		
-		for (int i = n + 1; i <= 2 * n; i++)
-			if (list[i]) cnt++;
-
-		cout << cnt << "\n";
+		std::cout << cnt << "\n";
 	}
-	return 0;
 }
