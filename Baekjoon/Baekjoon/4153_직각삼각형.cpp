7124c06d4a575b393634b2f12da85ce2db241e00
ije000131@gmail.com <ije000131@gmail.com>
2021-02-14 15:08:19 +0900

    algo 수정

--- Baekjoon/Baekjoon/4153_직각삼각형.cpp
+++ Baekjoon/Baekjoon/4153_직각삼각형.cpp
@@ -2,34 +2,21 @@
 #include <algorithm>
 using namespace std;
 
-void print(int max, int a, int b)
-{
-	if (max * max == (a * a) + (b * b))
-		cout << "right\n";
-	else
-		cout << "wrong\n";
-}
-
 int main()
 {
 	while (1)
 	{
 		int num[3];
 		cin >> num[0] >> num[1] >> num[2];
-
-		if (num[0] <= 0 && num[1] <= 0 && num[2] <= 0)
+		
+		if (!num[0] && !num[1] && !num[2])
 			break;
 		
-		int maxNum = num[0];
-		for (int i = 0; i < 3; i++)
-			maxNum = max(maxNum, num[i]);
-
-		if (maxNum == num[0])
-			print(maxNum, num[1], num[2]);
-		else if (maxNum == num[1])
-			print(maxNum, num[0], num[2]);
+		sort(num, num + 3);
+		if ((num[2] * num[2]) == (num[0] * num[0]) + (num[1] * num[1]))
+			cout << "right\n";
 		else
-			print(maxNum, num[0], num[1]);
+			cout << "wrong\n";
 	}
 
 	return 0;
