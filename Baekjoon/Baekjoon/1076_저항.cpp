4dff7b3dd358897ca201fc46eec3ad413879ca30
Ljieun <44721525+Ljieun@users.noreply.github.com>
2021-03-19 00:08:00 +0900

    algo 수정

--- Baekjoon/Baekjoon/1076_저항.cpp
+++ Baekjoon/Baekjoon/1076_저항.cpp
@@ -1,74 +1,17 @@
 #include <iostream>
-#include <string>
+#include <map>
+#include <math.h>
 using namespace std;
 
 int main() {
-	string color, result;
-	int idx, mul;
-
-	for (int i = 0; i < 3; i++)
+	string c1, c2, c3;
+	map<string, int> map = 
 	{
-		cin >> color;
-		if (color == "black")
-		{
-			idx = 0;
-			mul = 1;
-		}
-		else if (color == "brown")
-		{
-			idx = 1;
-			mul = 10;
-		}
-		else if (color == "red")
-		{
-			idx = 2;
-			mul = 100;
-		}
-		else if (color == "orange")
-		{
-			idx = 3;
-			mul = 1000;
-		}
-		else if (color == "yellow")
-		{
-			idx = 4;
-			mul = 10000;
-		}
-		else if (color == "green")
-		{
-			idx = 5;
-			mul = 100000;
-		}
-		else if (color == "blue")
-		{
-			idx = 6;
-			mul = 1000000;
-		}
-		else if (color == "violet")
-		{
-			idx = 7;
-			mul = 10000000;
-		}
-		else if (color == "grey")
-		{
-			idx = 8;
-			mul = 100000000;
-		}
-		else if (color == "white")
-		{
-			idx = 9;
-			mul = 1000000000;
-		}
+		{"brown", 1}, {"red", 2}, {"orange", 3}, {"yellow", 4}, {"green", 5},
+		{"blue", 6}, {"violet", 7}, {"grey", 8}, {"white", 9}
+	};
 
-		if (i < 2)
-		{
-			if (i == 0 && idx == 0)
-				continue;
-
-			result += to_string(idx);
-		}
-		else
-			cout << stoll(result) * mul << "\n";
-	}
+	cin >> c1 >> c2 >> c3;
+	cout << (map[c1] * 10 + map[c2]) * (long long)pow(10, map[c3]);
 	return 0;
 }
