c7ac738b86a6fd9bf3f7593750a6ca2929f0e534
Ljieun <44721525+Ljieun@users.noreply.github.com>
2021-03-27 14:24:29 +0900

    algo 수정

--- Baekjoon/Baekjoon/1100_하얀_칸.cpp
+++ Baekjoon/Baekjoon/1100_하얀_칸.cpp
@@ -4,17 +4,12 @@ using namespace std;
 int main() {
 	string line;
 	int cnt = 0;
+
 	for (int i = 0; i < 8; i++) {
 		cin >> line;
-		for (int j = 0; j < 4; j++) {
-			if (i % 2 != 0) {
-				if (line.at(2 * j + 1) == 'F')
-					cnt++;
-			}
-			else {
-				if (line.at(2 * j) == 'F')
-					cnt++;
-			}
+		for (int j = 0; j < 8; j++) {
+			if ((line[j] == 'F') && ((i + j) % 2 == 0))
+				cnt++;
 		}
 	}
 	cout << cnt;
