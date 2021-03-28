f738bafcf1cce0340521895acd11fc081179d1bd
Ljieun <44721525+Ljieun@users.noreply.github.com>
2021-03-28 22:32:57 +0900

    algo 수정

--- Baekjoon/Programmers/크레인_인형뽑기_게임.cpp
+++ Baekjoon/Programmers/크레인_인형뽑기_게임.cpp
@@ -5,28 +5,24 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
 	int answer = 0;
-	int N = board[0].size();
 	vector<int> basket;
 
-	// 1. 바구니에 넣기
 	for (int i = 0; i < moves.size(); i++) {
-		for (int j = 0; j < N; j++) {
-			int a = moves[i] - 1;
-			if (board[j][a] == 0) continue;
-			basket.push_back(board[j][a]);
-			board[j][a] = 0;
+		for (int j = 0; j < board.size(); j++) {
+			int idx = moves[i] - 1;
+			if (board[j][idx] == 0) continue;
+
+			if (!basket.empty() && basket.back() == board[j][idx]) {
+				basket.pop_back();
+				answer += 2;
+			}
+			else
+				basket.push_back(board[j][idx]);
+
+			board[j][idx] = 0;
 			break;
 		}
-
-		// 2. 바구니 체크
-		int k = basket.size();
-		if ((k > 1) && (basket[k - 1] == basket[k - 2])) {
-			basket.pop_back();
-			basket.pop_back();
-			answer += 2;
-		}
 	}
-
 	return answer;
 }
 
