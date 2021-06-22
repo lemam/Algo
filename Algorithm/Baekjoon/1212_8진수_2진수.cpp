9f39e098e41a786923cb075723df722f22f4c1e4
Ljieun <44721525+Ljieun@users.noreply.github.com>
2021-06-22 19:06:16 +0900

    Binary number and Octal number

--- Algorithm/Baekjoon/1212_8진수_2진수.cpp
+++ Algorithm/Baekjoon/1212_8진수_2진수.cpp
@@ -2,15 +2,54 @@
 
 using namespace std;
 
-int main() {
+class Binary {
+	string fisrtBinary[8] = { "0", "1", "10", "11", "100","101", "110", "111" };
+	string extraBinary[8] = { "000", "001", "010", "011", "100", "101", "110", "111" };
+
 	string input;
+	string result;
 
-	// 8진수 입력 받기
-	cin >> input;
+public:
+	Binary() {
+		cin >> input;
+	}
 
-	// 한 자리씩 2진수로 바꾸기
+	void convertNumber();
+	string getFirstNum(int idx);
+	string getExtraNum(int idx);
+	void print();
+};
 
-	// 2진수 출력
+void Binary::convertNumber() {
+	// 각 자리 수의 2진수 세자리 구하기
+	// 값이 0으로 시작하지 않도록 한다.
+
+	int length = static_cast<int>(input.length());
+
+	result += getFirstNum(input[0] - '0');
+
+	for (int i = 1; i < length; i++) {
+		result += getExtraNum(input[i] - '0');
+	}
+}
+
+string Binary::getFirstNum(int idx) {
+	return fisrtBinary[idx];
+}
+
+string Binary::getExtraNum(int idx) {
+	return extraBinary[idx];
+}
+
+void Binary::print() {
+	cout << result;
+}
+
+
+int main() {
+	Binary oct;
+	oct.convertNumber();
+	oct.print();
 
 	return 0;
 }
