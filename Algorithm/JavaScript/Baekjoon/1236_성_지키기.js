695523c71f2a17ae3461da7a15ad6b38d662a3d7
Ljieun <44721525+Ljieun@users.noreply.github.com>
2021-10-18 01:20:11 +0900

    1236 : Use reduce method

--- Algorithm/JavaScript/Baekjoon/1236_성_지키기.js
+++ Algorithm/JavaScript/Baekjoon/1236_성_지키기.js
@@ -2,24 +2,14 @@ const fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const castleSize = input[0].split(" ");
-const N = parseInt(castleSize[0]);
-const M = parseInt(castleSize[1]);
+const N = parseInt(castleSize[0]); // row number
+const M = parseInt(castleSize[1]); // col number
 
 let line = [];
 let guardCol = new Array(M).fill(true);
 let guardRow = new Array(N).fill(true);
 let guardColSeat = 0;
 let guardRowSeat = 0;
-let answer = 0;
-
-function CountSeat(arr) {
-  let cnt = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) cnt++;
-  }
-
-  return cnt;
-}
 
 for (let i = 1; i <= N; i++) {
   for (let j = 0; j < M; j++) {
@@ -32,13 +22,11 @@ for (let i = 1; i <= N; i++) {
   line = [];
 }
 
-guardColSeat = CountSeat(guardCol);
-guardRowSeat = CountSeat(guardRow);
+guardColSeat = guardCol.reduce(function (pv, cv) {
+  return pv + cv;
+});
+guardRowSeat = guardRow.reduce(function (pv, cv) {
+  return pv + cv;
+});
 
-if (guardColSeat < guardRowSeat) {
-  answer = guardColSeat + (guardRowSeat - guardColSeat);
-} else {
-  answer = guardRowSeat + (guardColSeat - guardRowSeat);
-}
-
-console.log(answer);
+console.log(Math.max(guardColSeat, guardRowSeat));
