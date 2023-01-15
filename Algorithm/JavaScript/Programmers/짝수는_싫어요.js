3cc83603ff055dea79fd3dc6a2586df1d6201836
lemam <ije000131@gmail.com>
2023-01-15 19:34:41 +0900

    fix: i += 2

--- Algorithm/JavaScript/Programmers/짝수는_싫어요.js
+++ Algorithm/JavaScript/Programmers/짝수는_싫어요.js
@@ -1,8 +1,7 @@
 function solution(n) {
   var answer = [];
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 !== 0) answer.push(i);
-  }
+
+  for (let i = 1; i <= n; i += 2) answer.push(i);
 
   return answer;
 }
