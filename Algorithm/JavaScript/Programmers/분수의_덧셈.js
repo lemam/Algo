ffc7fc37f3aff139652c9e0d2ef6dab7e9f2812f
lemam <ije000131@gmail.com>
2023-01-15 16:27:42 +0900

    fix: 유클리드 호제법

--- Algorithm/JavaScript/Programmers/분수의_덧셈.js
+++ Algorithm/JavaScript/Programmers/분수의_덧셈.js
@@ -1,22 +1,11 @@
+function fnGCD(a, b) {
+  return b ? fnGCD(b, a % b) : a;
+}
+
 function solution(numer1, denom1, numer2, denom2) {
-  let numer = 0;
-  let denom = 0;
+  let numer = numer1 * denom2 + numer2 * denom1;
+  let denom = denom1 * denom2;
+  let gcd = fnGCD(numer, denom);
 
-  if (denom1 === denom2) {
-    numer = numer1 + numer2;
-    denom = denom1;
-  } else {
-    numer = numer1 * denom2 + numer2 * denom1;
-    denom = denom1 * denom2;
-  }
-
-  for (let i = 2; i <= denom; ) {
-    if (numer % i == 0 && denom % i == 0) {
-      numer /= i;
-      denom /= i;
-    } else i++;
-  }
-
-  var answer = [numer, denom];
-  return answer;
+  return [numer / gcd, denom / gcd];
 }
