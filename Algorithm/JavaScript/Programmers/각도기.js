9fba4c0eb3754d4f693a9552cb9a99dc3377c991
lemam <ije000131@gmail.com>
2023-01-17 22:55:07 +0900

    fix: Array filter

--- Algorithm/JavaScript/Programmers/각도기.js
+++ Algorithm/JavaScript/Programmers/각도기.js
@@ -1,6 +1,3 @@
 function solution(angle) {
-  if (angle > 0 && angle < 90) return 1;
-  else if (angle == 90) return 2;
-  else if (angle > 90 && angle < 180) return 3;
-  else return 4;
+  return [0, 90, 91, 180].filter(x => angle >= x).length;
 }
