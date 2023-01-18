d915ea28ed63df4a51ed3667e6a7f53e70e92ef7
lemam <ije000131@gmail.com>
2023-01-18 22:58:16 +0900

    fix: 구조분해, Array map

--- Algorithm/JavaScript/Programmers/진료_순서_정하기.js
+++ Algorithm/JavaScript/Programmers/진료_순서_정하기.js
@@ -1,11 +1,5 @@
 function solution(emergency) {
-  var answer = [];
-  let arr = emergency.slice().sort((a, b) => b - a);
-  let order = 1;
+  const sorted = [...emergency].sort((a, b) => b - a);
 
-  for (n of arr) {
-    answer[emergency.findIndex(el => el == n)] = order++;
-  }
-
-  return answer;
+  return emergency.map(x => sorted.indexOf(x) + 1);
 }
