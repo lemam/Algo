ea3538fbec6a1b4784b47ec0ec83767f3d9a72fa
lemam <ije000131@gmail.com>
2023-06-21 18:01:31 +0900

    add: 구조 분해 할당

--- Algorithm/JavaScript/Programmers/인덱스_바꾸기.js
+++ Algorithm/JavaScript/Programmers/인덱스_바꾸기.js
@@ -1,11 +1,19 @@
+// my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 반환하기
+
+// param {String} my_string
+// param {Number} num1
+// param {Number} num2
+// return {String} my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열
+
+// example
+// my_string : "hello"
+// num1      : 1
+// num2      : 2
+// return    : "hlelo"
+
 function solution(my_string, num1, num2) {
-  let answer = [...my_string];
+  my_string = my_string.split("");
+  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
 
-  answer.splice(num1, 0, my_string[num2]);
-  answer.splice(num1 + 1, 1);
-
-  answer.splice(num2, 0, my_string[num1]);
-  answer.splice(num2 + 1, 1);
-
-  return answer.join("");
+  return my_string.join("");
 }
