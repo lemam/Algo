5afbab120b3219444d91ad05b35c86e6ad4d22aa
lemam <ije000131@gmail.com>
2023-06-21 19:26:10 +0900

    add: 정규표현식

--- Algorithm/JavaScript/Programmers/한_번만_등장한_문자.js
+++ Algorithm/JavaScript/Programmers/한_번만_등장한_문자.js
@@ -1,13 +1,16 @@
+// s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 반환하기
+// 한 번만 등장하는 문자가 없을 경우 빈 문자열을 반환한다.
+
+// param {String} s
+// return {String} s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열 또는 빈 문자열
+
+// example
+// s      = "abcabcadc"
+// result = "d"
+
 function solution(s) {
-  let answer = [];
-  const charMap = new Map();
-
-  [...s].map(x => charMap.set(x, charMap.get(x) + 1 || 1));
-
-  for (let key of charMap.keys()) {
-    if (charMap.get(key) > 1) continue;
-    answer.push(key);
-  }
-
-  return answer.sort().join("");
+  return [...s]
+    .filter(ch => s.match(new RegExp(ch, "g")).length === 1)
+    .sort()
+    .join();
 }
