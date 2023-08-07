103033f4d9d54402adea9a9dde248a8c09eb4442
sohyun.park <s_971001@naver.com>
2023-08-07 17:11:14 +0900

    백준 심화1

---
+++ Algorithm/Java/Baekjoon/BOJ_25083_새싹.java
@@ -0,0 +1,17 @@
+
+/*
+ * 归霖 25083锅 - 货较 (宏沸令5)
+ * https://www.acmicpc.net/problem/25083
+ * 
+ * 货较 免仿
+ */
+public class BOJ_25083_货较 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
---
+++ Algorithm/Java/Baekjoon/BOJ_3003_킹_퀸_룩_비숍_나이트_폰.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 백준 3003번 - 킹, 퀸, 룩, 비숍, 나이트, 폰 (브론즈 5)
+ * 
+ * 낡은 체스판과 피스를 발견했다.
+ * 검정색 피스는 모두 있었으나, 흰색 피스는 개수가 올바르지 않았다.
+ * 
+ * 체스는 총 16개의 피스를 사용하며, 
+ * 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
+ * 
+ * 발견한 흰색 피스의 개수가 주어졌을 때, 
+ * 몇 개를 더하거나 빼야 올바른 세트가 되는지 구하는 프로그램을 작성하시오.
+ */
+
+public class BOJ_3003_킹_퀸_룩_비숍_나이트_폰 {
+	public static void main(String[] args) throws IOException {
+		int[] black = { 1, 1, 2, 2, 2, 8 };
+		int[] white = new int[5];
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+
+		for (int i = 0; i < white.length; i++) {
+			white[i] = Integer.parseInt(input[i]);
+		}
+
+		br.close();
+	}
+}
