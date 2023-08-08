d0464f6d7c0f03edd9e65b507066ac16722e75a3
jieun.lim <ije000131@gmail.com>
2023-08-08 17:02:59 +0900

    파일 이동

---
+++ Algorithm/Java/Baekjoon/BOJ_8393_합.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_8393_합 {
+	public static void main(String[] args) throws IOException {
+		/*
+		 * n이 주어졌을 때, 1부터 n까지의 합
+		 */
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n * (n + 1) / 2);
+	}
+}
