b85aeaf4ea2416eb63b6eca376eb5e8b8f644e07
lemam <ije000131@gmail.com>
2023-10-18 22:35:24 +0900

    SWEA 트리

--- Algorithm/Java/SWEA/SWEA_1232_사칙연산.java
+++ Algorithm/Java/SWEA/SWEA_1232_사칙연산.java
@@ -1,70 +1,67 @@
-import java.util.Scanner;
-import java.util.Stack;
-
 /*
  * SWEA 1232번 - 사칙연산 (D4)
  * 
- * 사칙연산으로 구성되어 있는 식은 이진 트리로 표현할 수 있다.
+ * 사칙연산으로 구성되어 있는 식은 이진 트리로 표현할 수 있다. 
+ * 아래는 식 “(9/(6-4))*3”을 이진 트리로 표현한 것이다.
  * 임의의 정점에 연산자가 있으면 해당 연산자의 왼쪽 서브 트리의 결과와 오른쪽 서브 트리의 결과에 해당 연산자를 적용한다.
  * 
- * 사칙연산 “+, -, *, /”와 양의 정수로만 구성된 임의의 이진 트리가 주어질 때, 이를 계산한 결과를 출력하는 프로그램을 작성하라.
+ * 사칙연산 “+, -, *, /”와 양의 정수로만 구성된 임의의 이진 트리가 주어질 때, 
+ * 이를 계산한 결과를 출력하는 프로그램을 작성하라.
  * 계산 중간 과정에서의 연산은 모두 실수 연산으로 한다.
  * 
- * 정점이 정수면 정점 번호와 양의 정수가 주어지고, 정점이 연산자이면 정점 번호, 연산자, 해당 정점의 왼쪽 자식, 오른쪽 자식의 정점 번호가 차례대로 주어진다.
- * 정점 번호는 1부터 N까지의 정수로 구분되고 루트 정점의 번호는 항상 1이다.
+ * 정점 번호는 1부터 N까지의 정수로 구분된고 루트 정점의 번호는 항상 1이다.
  */
+
+import java.util.Scanner;
+import java.util.Stack;
 
 public class SWEA_1232_사칙연산 {
 	private static class Node {
-		public String data; // 데이터
-		public int left; // 왼쪽 자식 노드 번호
-		public int right; // 오른쪽 자식 노드 번호
+		String data;
+		int left, right;
 
 		public Node() {
 		}
 	}
 
-	public static Node[] nodes;
-	public static Stack<String> stack;
+	private static Node[] tree;
+	private static Stack<String> stack;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int T = 10; // 테스트 케이스
+
+		int T = 10; // 테스트 케이스 수
 
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(sc.nextLine());
-			nodes = new Node[N + 1];
+			int N = Integer.parseInt(sc.nextLine()); // 정점의 개수
+			tree = new Node[N + 1]; // 트리 배열
 
-			// 노드 생성
-			for (int i = 1; i <= N; i++) {
-				nodes[i] = new Node();
+			// 정점 초기화
+			for (int i = 1; i < N + 1; i++) {
+				tree[i] = new Node();
 			}
 
-			// 트리 생성
+			// 정점 입력
 			for (int i = 1; i <= N; i++) {
-				String[] str = sc.nextLine().split(" ");
-				int num = Integer.parseInt(str[0]); // 정점 번호
-				String data = str[1]; // 정점 데이터
+				String[] input = sc.nextLine().split(" ");
+				int num = Integer.parseInt(input[0]); // 정점 번호
 
-				// 데이터 추가
-				nodes[num].data = data;
+				tree[num].data = input[1]; // 값
 
-				if (str.length >= 3) {
-					// 왼쪽 자식 노드 연결
-					int left = Integer.parseInt(str[2]);
-					nodes[num].left = left;
+				// data가 연산자이면 왼쪽 자식, 오른쪽 자식의 정점 번호 저장
+				if (input.length >= 3) {
+					int left = Integer.parseInt(input[2]);
+					tree[num].left = left;
 
-					// 오른쪽 자식 노드 연결
-					if (str.length == 4) {
-						int right = Integer.parseInt(str[3]);
-						nodes[num].right = right;
-					}
+					int right = Integer.parseInt(input[3]);
+					tree[num].right = right;
 				}
 			}
 
-			stack = new Stack<String>();
+			stack = new Stack<>(); // 후위 표기법 계산을 위한 스택
 
-			postorder(1);
+			postorder(1); // 후위 순회
 
 			System.out.printf("#%d %s\n", tc, stack.pop());
 		}
@@ -72,45 +69,45 @@ public class SWEA_1232_사칙연산 {
 		sc.close();
 	}
 
-	// 후위 순회
 	private static void postorder(int i) {
-		Node node = nodes[i];
+		Node curr = tree[i];
 
-		if (nodes[node.left] != null)
-			postorder(node.left);
+		if (tree[curr.left] != null)
+			postorder(curr.left);
 
-		if (nodes[node.right] != null)
-			postorder(node.right);
+		if (tree[curr.right] != null)
+			postorder(curr.right);
 
-		calc(node);
+		calc(curr); // 계산
 	}
 
-	// 후위 표기법 계산
 	private static void calc(Node node) {
-		// 연산자이면 스택에서 두 개의 값을 꺼내 연산한 후 다시 스택에 넣는다.
-		if (node.data.equals("+")) {
-			int B = Integer.parseInt(stack.pop());
-			int A = Integer.parseInt(stack.pop());
+		int A, B;
 
+		// 연산자이면 스택에서 두 개의 값을 꺼내 계산한 후 결과값을 스택에 넣는다.
+		switch (node.data) {
+		case "+":
+			B = Integer.parseInt(stack.pop());
+			A = Integer.parseInt(stack.pop());
 			stack.push(Integer.toString(A + B));
-		} else if (node.data.equals("-")) {
-			int B = Integer.parseInt(stack.pop());
-			int A = Integer.parseInt(stack.pop());
-
+			break;
+		case "-":
+			B = Integer.parseInt(stack.pop());
+			A = Integer.parseInt(stack.pop());
 			stack.push(Integer.toString(A - B));
-		} else if (node.data.equals("*")) {
-			int B = Integer.parseInt(stack.pop());
-			int A = Integer.parseInt(stack.pop());
-
+			break;
+		case "*":
+			B = Integer.parseInt(stack.pop());
+			A = Integer.parseInt(stack.pop());
 			stack.push(Integer.toString(A * B));
-		} else if (node.data.equals("/")) {
-			int B = Integer.parseInt(stack.pop());
-			int A = Integer.parseInt(stack.pop());
-
+			break;
+		case "/":
+			B = Integer.parseInt(stack.pop());
+			A = Integer.parseInt(stack.pop());
 			stack.push(Integer.toString(A / B));
-		}
-		// 피연산자이면 스택에 넣는다.
-		else {
+			break;
+		// 양의 정수인 경우 스택에 넣는다.
+		default:
 			stack.push(node.data);
 		}
 	}
