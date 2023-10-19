065dae591fd802fae5159c2a7c5e06d74e5d02a8
lemam <ije000131@gmail.com>
2023-10-19 22:20:56 +0900

    SWEA 트리

--- Algorithm/Java/SWEA/SWEA_1248_공통조상.java
+++ Algorithm/Java/SWEA/SWEA_1248_공통조상.java
@@ -1,102 +1,104 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * SWEA 1248번 - 공통조상 (D5)
  * 
  * 이진 트리에서 임의의 두 정점의 가장 가까운 공통 조상을 찾고, 
  * 그 정점을 루트로 하는 서브 트리의 크기를 알아내는 프로그램을 작성하라.
+ * 
+ * 정점의 번호는 1부터 V까지의 정수이며, 루트 정점은 항상 1번이다.
+ * 가장 가까운 공통 조상의 번호와 그것을 루트로 하는 서브 트리의 크기를 공백으로 구분하여 출력하라.
  */
-
-import java.util.Scanner;
 
 public class SWEA_1248_공통조상 {
 	private static class Node {
-		public int parent;
-		public int left;
-		public int right;
+		int parent, left, right;
 
 		public Node() {
 		}
 	}
 
-	public static Node[] nodes; // 정점 노드 배열
-	public static boolean[] visited; // 마킹 배열
-	public static int ancestor; // 공통 조상
-	public static int subTreeSize; // 공통 조상을 루트로 하는 서브 트리의 크기
+	private static Node[] nodeArr;
+	private static int ancestor, subTreeSize;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); // 테스트 케이스
+
+		int T = sc.nextInt(); // 테스트케이스의 수
 
 		for (int tc = 1; tc <= T; tc++) {
 			int V = sc.nextInt(); // 정점의 개수
 			int E = sc.nextInt(); // 간선의 개수
-			int N1 = sc.nextInt(); // 공통 조상을 찾는 두 개의 정점 번호 1
-			int N2 = sc.nextInt(); // 공통 조상을 찾는 두 개의 정점 번호 2
 
-			nodes = new Node[V + 1];
+			// 공통 조상을 찾는 두 개의 정점 번호
+			int A = sc.nextInt();
+			int B = sc.nextInt();
 
-			// 노드 생성
-			for (int i = 1; i <= V; i++) {
-				nodes[i] = new Node();
-			}
+			nodeArr = new Node[V + 1]; // 트리 배열
 
-			// 트리 생성
+			// 트리 배열 초기화
+			for (int i = 0; i < V + 1; i++)
+				nodeArr[i] = new Node();
+
+			// 정점 입력
 			for (int i = 0; i < E; i++) {
-				int parent = sc.nextInt(); // 부모 노드 번호
-				int child = sc.nextInt(); // 자식 노드 번호
-
+				int pNum = sc.nextInt(); // 부모 정점 번호
+				int cNum = sc.nextInt(); // 자식 정점 번호
+				
 				// 자식 노드 연결
-				if (nodes[parent].left == 0)
-					nodes[parent].left = child;
+				if (nodeArr[pNum].left == 0)
+					nodeArr[pNum].left = cNum;
 				else
-					nodes[parent].right = child;
+					nodeArr[pNum].right = cNum;
 
 				// 부모 노드 연결
-				nodes[child].parent = parent;
+				nodeArr[cNum].parent = pNum;
+				
 			}
+			
+			boolean[] visited = new boolean[V + 1]; // 노드 방문 배열
+			ancestor = 0; // 공통 조상 정점의 번호
+			subTreeSize = 0; // 공통 조상을 루트로 하는 서브 트리의 크기
 
-			visited = new boolean[V + 1];
-			ancestor = 1;
-			subTreeSize = 0;
-
-			// N1과 N2의 공통 조상 찾기
-			findAncestor(N1);
-			findAncestor(N2);
-
-			// 공통 조상을 루트로 하는 서브 트리의 크기 저장
-			getSubTreeSize(nodes[ancestor]);
-
+			findAncestor(A, visited);
+			findAncestor(B, visited);
+			
+			getSubTreeSize(nodeArr[ancestor]);
+			
 			System.out.printf("#%d %d %d\n", tc, ancestor, subTreeSize);
 		}
 
 		sc.close();
 	}
 
-	// 공통 조상 찾기
-	// 공통 조상을 찾고자 하는 정점에서 부모 노드를 따라 찾아가며 마킹한다.
-	// 또 다른 정점의 탐색 과정에서 이미 마킹된 부모 노드를 만났다면 그 노드가 공통 조상이다.
-	private static void findAncestor(int num) {
-		int curr = num;
+	// node : 시작 노드
+	// node를 root로 하는 서브 트리를 순회하며 정점의 개수를 subTreeSize에 저장한다.
+	private static void getSubTreeSize(Node node) {
+		subTreeSize++;
+		
+		if (node.left != 0)
+			getSubTreeSize(nodeArr[node.left]);
+		
+		if (node.right != 0)
+			getSubTreeSize(nodeArr[node.right]);
+	}
 
-		while (curr > 1) {
-			int parent = nodes[curr].parent;
+	private static void findAncestor(int n, boolean[] visited) {
+		int child = n;
 
-			// 마킹한 부모 노드이면 공통 조상이다.
+		while (child > 1) {
+			int parent = nodeArr[child].parent;
+
+			// 이미 방문한 정점이라면 이 정점이 공통조상이다.
 			if (visited[parent]) {
 				ancestor = parent;
 				break;
 			}
 
 			visited[parent] = true;
-			curr = parent;
+			child = parent;
 		}
 	}
 
-	// 공통 조상을 루트로 하는 서브 트리의 크기를 센다.
-	private static void getSubTreeSize(Node node) {
-		subTreeSize++;
-		if (nodes[node.left] != null)
-			getSubTreeSize(nodes[node.left]);
-		if (nodes[node.right] != null)
-			getSubTreeSize(nodes[node.right]);
-	}
 }
