7f251304b70844f1d2dc02617a1060d1ee79a6f5
Jieun Lim <44721525+lemam@users.noreply.github.com>
2024-03-06 22:22:23 +0900

    백준 14502 연구소

--- Algorithm/Java/Baekjoon/BOJ_14502_연구소.java
+++ Algorithm/Java/Baekjoon/BOJ_14502_연구소.java
@@ -6,115 +6,107 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_14502_연구소 {
-	private static int N, M;
-	private static int[][] map;
-	private static int answer;
+    private static int N, M, answer;
+    private static int[][] map;
 
-	// 12 3 6 9
-	private static int[] dr = { -1, 0, 1, 0 };
-	private static int[] dc = { 0, 1, 0, -1 };
+    // 12, 3, 6, 9
+    private static int[] dr = {-1, 0, 1, 0};
+    private static int[] dc = {0, 1, 0, -1};
 
-	private static class Node {
-		int row, col;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		public Node(int x, int y) {
-			this.row = x;
-			this.col = y;
-		}
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        answer = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		answer = 0;
+        selectWalls(0);
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        System.out.println(answer);
 
-		dfs(0);
-		
-		System.out.println(answer);
+        br.close();
+    }
 
-		br.close();
-	}
+    private static void selectWalls(int wallCnt) {
+        // 벽 3개를 설치하면 안전 구역을 탐색한다.
+        if (wallCnt == 3) {
+            countSafeArea();
+            return;
+        }
 
-	private static void dfs(int wallCnt) {
-		// 벽 3개를 설치하면 안전구역을 탐색한다.
-		if (wallCnt == 3) {
-			bfs();
-			return;
-		}
+        // 벽 설치하기
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j] = 1;  // 벽 설치
+                    selectWalls(wallCnt + 1);
+                    map[i][j] = 0;  // 원상복구
+                }
+            }
+        }
+    }
 
-		// 벽 설치하기
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 0) {
-					map[i][j] = 1; // 벽 설치
-					dfs(wallCnt + 1);
-					map[i][j] = 0; // 원상복구
-				}
-			}
-		}
-	}
+    private static void countSafeArea() {
+        Queue<int[]> queue = new LinkedList<>();
 
-	private static void bfs() {
-		Queue<Node> queue = new LinkedList<Node>();
+        // 바이러스 칸을 큐에 넣는다.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
 
-		// 바이러스 칸을 큐에 넣는다.
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 2) {
-					queue.add(new Node(i, j));
-				}
-			}
-		}
+        // map 배열 복사
+        int[][] copyMap = new int[N][M];
 
-		// map 배열 복사
-		int[][] copyMap = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            copyMap[i] = map[i].clone();
+        }
 
-		for (int i = 0; i < N; i++) {
-			copyMap[i] = map[i].clone();
-		}
+        // BFS 탐색
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int row = now[0];
+            int col = now[1];
 
-		// BFS 탐색
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nr = row + dr[dir];
+                int nc = col + dc[dir];
 
-			for (int d = 0; d < 4; d++) {
-				int nr = now.row + dr[d];
-				int nc = now.col + dc[d];
+                // 연구소 범위 밖이거나 빈칸이 아니면 건너뛴다.
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || copyMap[nr][nc] != 0) {
+                    continue;
+                }
 
-				// 연구소 범위 밖이거나 빈칸이 아니면 스킵한다.
-				if (nr < 0 || nr >= N || nc < 0 || nc >= M || copyMap[nr][nc] != 0) {
-					continue;
-				}
+                // 바이러스 퍼뜨리기
+                queue.add(new int[]{nr, nc});
+                copyMap[nr][nc] = 2;
+            }
+        }
 
-				// 바이러스 퍼뜨리기
-				queue.add(new Node(nr, nc));
-				copyMap[nr][nc] = 2;
-			}
-		}
+        // 안전구역 개수 세기
+        int safeCnt = 0;
 
-		// 안전구역 개수 확인하기
-		int safeCnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (copyMap[i][j] == 0) {
+                    safeCnt++;
+                }
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (copyMap[i][j] == 0) {
-					safeCnt++;
-				}
-			}
-		}
-
-		// 최댓값 갱신
-		answer = Math.max(safeCnt, answer);
-	}
+        // 최댓값 갱신
+        answer = Math.max(safeCnt, answer);
+    }
 }
