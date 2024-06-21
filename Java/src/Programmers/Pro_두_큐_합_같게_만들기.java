3e045085cfe7751e8c983355eed0ddac324023ad
lemam <ije000131@gmail.com>
2024-06-21 09:50:37 +0900

    프로그래머스 두 큐 합 같게 만들기

--- Java/src/Programmers/Pro_두_큐_합_같게_만들기.java
+++ Java/src/Programmers/Pro_두_큐_합_같게_만들기.java
@@ -19,7 +19,7 @@ public class Pro_두_큐_합_같게_만들기 {
         if ((sum1 + sum2) % 2 != 0) return -1;
 
         while (sum1 != sum2) {
-            // (큐1 + 큐2) * 2 == 한 큐의 길이 * 2
+            // (큐1 + 큐2) * 2 == 한 큐의 길이 * 4
             // 답이 나오지 않는 최악의 경우에 양쪽 큐가 원래의 상태로 돌아오기까지 필요한 횟수
             if (answer > (queue1.length + queue2.length) * 2) {
                 return -1;
