b5e2d3143b90898009b20ae19a8b5770d0e479df
lemam <ije000131@gmail.com>
2024-07-14 21:00:52 +0900

    [Java] 프로그래머스 개인정보 수집 유효기간

--- Java/src/Programmers/Pro_개인정보_수집_유효기간.java
+++ Java/src/Programmers/Pro_개인정보_수집_유효기간.java
@@ -6,50 +6,54 @@ import java.util.List;
 import java.util.Map;
 
 public class Pro_개인정보_수집_유효기간 {
-	public static int[] solution(String today, String[] terms, String[] privacies) {
-		List<Integer> answerList = new ArrayList<Integer>();
-		int tDate = getDate(today);
+    public class Date {
+        int year;
+        int month;
+        int day;
 
-		// 약관 정보를 Map에 저장한다.
-		Map<String, Integer> termMap = new HashMap<String, Integer>();
+        public Date(String str) {
+            String[] arr = str.split("\\.");
+            this.year = Integer.parseInt(arr[0]);
+            this.month = Integer.parseInt(arr[1]);
+            this.day = Integer.parseInt(arr[2]);
+        }
 
-		for (String term : terms) {
-			String[] arr = term.split(" ");
-			termMap.put(arr[0], Integer.parseInt(arr[1]));
-		}
+        public int getDayInDate() {
+            return this.year * 12 * 28 + this.month * 28 + this.day;
+        }
+    }
 
-		// 개인정보 배열 탐색
-		for (int i = 0; i < privacies.length; i++) {
-			String[] privacyArr = privacies[i].split(" ");
-			int pDate = getDate(privacyArr[0]);
-			String type = privacyArr[1]; // 약관 종류
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> result = new ArrayList<>();   // 파기해야할 개인정보 번호를 담을 리스트
+        Date todayDate = new Date(today);   // 오늘 날짜 객체
 
-			// 해당 약관의 유효기간을 개인정보 수집 일자에 더한다.
-			pDate += termMap.get(type) * 28;
+        // 유효기간 저장
+        Map<String, Integer> termMap = new HashMap<>(); // 유효기간을 담을 map {종류=기간}
+        for (String term : terms) {
+            String[] arr = term.split(" ");
+            termMap.put(arr[0], Integer.parseInt(arr[1]));
+        }
 
-			// 개인정보 수집 일자가 오늘 날짜로 유효하면 개인정보 번호를 저장한다.
-			if (pDate <= tDate) {
-				answerList.add(i + 1);
-			}
-		}
+        // 수집된 개인정보 탐색
+        for (int i = 0; i < privacies.length; i++) {
+            String[] arr = privacies[i].split(" ");
+            Date privacyDate = new Date(arr[0]);    // 수집된 날짜 객체
 
-		// List를 배열로 변환한다.
-		int len = answerList.size();
-		int[] answer = new int[len];
+            // 약관 종류에 따라 파기 날짜 계산
+            int deadline = privacyDate.getDayInDate() + termMap.get(arr[1]) * 28;
 
-		for (int i = 0; i < len; i++) {
-			answer[i] = answerList.get(i);
-		}
+            // 파기 날짜보다 현재 날짜가 같거나 크면 개인정보 파기
+            if (deadline <= todayDate.getDayInDate()) {
+                result.add(i + 1);
+            }
+        }
 
-		return answer;
-	}
+        // List를 Array로 변환
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i);
+        }
 
-	private static int getDate(String dateStr) {
-		String[] arr = dateStr.split("\\.");
-		int year = Integer.parseInt(arr[0]);
-		int month = Integer.parseInt(arr[1]);
-		int day = Integer.parseInt(arr[2]);
-
-		return (year * 12 * 28) + (month * 28) + day;
-	}
+        return answer;
+    }
 }
