e32657c0b762c8736ed5659de5c915c7bd38b563
lemam <ije000131@gmail.com>
2024-08-25 16:36:40 +0900

    [Java] 프로그래머스 가장 큰 수

---
+++ Java/src/Programmers/Pro_가장_큰_수.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+import java.util.*;
+
+class Pro_가장_큰_수 {
+    public static String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        // int[]는 Comparator를 사용할 수 없다!
+        Arrays.sort(arr, (o1, o2) -> ("" + o2 + o1).compareTo("" + o1 + o2));
+
+        // 0으로 시작하는 경우 0으로 반환
+        if (arr[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String str : arr) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
