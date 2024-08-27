a76931dd5faf698f3bf70c9459d98055c07f8fb2
lemam <ije000131@gmail.com>
2024-08-27 14:41:32 +0900

    [Java] 프로그래머스 가장 큰 수 - 필요없는 "" 삭제

--- Java/src/Programmers/Pro_가장_큰_수.java
+++ Java/src/Programmers/Pro_가장_큰_수.java
@@ -11,7 +11,7 @@ class Pro_가장_큰_수 {
         }
 
         // int[]는 Comparator를 사용할 수 없다!
-        Arrays.sort(arr, (o1, o2) -> ("" + o2 + o1).compareTo("" + o1 + o2));
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
         // 0으로 시작하는 경우 0으로 반환
         if (arr[0].equals("0")) return "0";
